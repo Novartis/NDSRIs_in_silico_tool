9fd64ef7efd56e72250e8ee8469d2d43b11dbc06
Jiazhou Zhu <jiazhoz@g.clemson.edu>
2023-09-18 13:46:04 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,16 +1,16 @@
-Copyright 2021 Novartis Institutes for BioMedical Research Inc.
+# Copyright 2021 Novartis Institutes for BioMedical Research Inc.
  
-Licensed under the MIT License (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+# Licensed under the MIT License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
  
-https://www.mit.edu/~amini/LICENSE.md
+# https://www.mit.edu/~amini/LICENSE.md
  
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import streamlit as st
 from rdkit import Chem
